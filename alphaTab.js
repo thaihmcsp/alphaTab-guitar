6b2b5e145e3ae0bc23fcd555af8ee3f65a9351b9
Thaihm <thaidd7b@gmail.com>
2021-05-23 16:16:38 +0700

    thai commit laptop

--- alphaTab.js
+++ alphaTab.js
@@ -4,7 +4,7 @@ const main = wrapper.querySelector(".at-main");
 
 // initialize alphatab
 const settings = {
-  file: "./Happy-Birthday-To-You-easy.gpx",
+  file: "./music/Happy-Birthday-To-You-easy.gpx",
   player: {
     enablePlayer: true,
     soundFont:
@@ -14,173 +14,207 @@ const settings = {
 };
 const api = new alphaTab.AlphaTabApi(main, settings);
 
-// overlay logic
-const overlay = wrapper.querySelector(".at-overlay");
-api.renderStarted.on(() => {
-  overlay.style.display = "flex";
-});
-api.renderFinished.on(() => {
-  overlay.style.display = "none";
-});
+function play() {
+  // overlay logic
+  const overlay = wrapper.querySelector(".at-overlay");
+  api.renderStarted.on(() => {
+    overlay.style.display = "flex";
+  });
+  api.renderFinished.on(() => {
+    overlay.style.display = "none";
+  });
 
-// track selector
-function createTrackItem(track) {
-  const trackItem = document
-    .querySelector("#at-track-template")
-    .content.cloneNode(true).firstElementChild;
-  trackItem.querySelector(".at-track-name").innerText = track.name;
-  trackItem.track = track;
-  trackItem.onclick = (e) => {
-    e.stopPropagation();
-    api.renderTracks([track]);
-  };
-  return trackItem;
-}
-const trackList = wrapper.querySelector(".at-track-list");
-api.scoreLoaded.on((score) => {
-  // clear items
-  trackList.innerHTML = "";
-  // generate a track item for all tracks of the score
-  score.tracks.forEach((track) => {
-    trackList.appendChild(createTrackItem(track));
+  // track selector
+  function createTrackItem(track) {
+    const trackItem = document
+      .querySelector("#at-track-template")
+      .content.cloneNode(true).firstElementChild;
+    trackItem.querySelector(".at-track-name").innerText = track.name;
+    trackItem.track = track;
+    trackItem.onclick = (e) => {
+      e.stopPropagation();
+      api.renderTracks([track]);
+    };
+    return trackItem;
+  }
+
+  const trackList = wrapper.querySelector(".at-track-list");
+  api.scoreLoaded.on((score) => {
+    // clear items
+    trackList.innerHTML = "";
+    // generate a track item for all tracks of the score
+    score.tracks.forEach((track) => {
+      trackList.appendChild(createTrackItem(track));
+    });
   });
-});
-api.renderStarted.on(() => {
-  // collect tracks being rendered
-  const tracks = new Map();
-  api.tracks.forEach((t) => {
-    tracks.set(t.index, t);
+
+  api.renderStarted.on(() => {
+    // collect tracks being rendered
+    const tracks = new Map();
+    api.tracks.forEach((t) => {
+      tracks.set(t.index, t);
+    });
+
+    // mark the item as active or not
+    const trackItems = trackList.querySelectorAll(".at-track");
+    trackItems.forEach((trackItem) => {
+      if (tracks.has(trackItem.track.index)) {
+        trackItem.classList.add("active");
+      } else {
+        trackItem.classList.remove("active");
+      }
+    });
   });
-  // mark the item as active or not
-  const trackItems = trackList.querySelectorAll(".at-track");
-  trackItems.forEach((trackItem) => {
-    if (tracks.has(trackItem.track.index)) {
-      trackItem.classList.add("active");
+
+  /** Controls **/
+
+  api.scoreLoaded.on((score) => {
+    wrapper.querySelector(".at-song-title").innerText = score.title;
+    wrapper.querySelector(".at-song-artist").innerText = score.artist;
+  });
+
+  const countIn = wrapper.querySelector(".at-controls .at-count-in");
+  countIn.onclick = () => {
+    countIn.classList.toggle("active");
+    if (countIn.classList.contains("active")) {
+      api.countInVolume = 1;
     } else {
-      trackItem.classList.remove("active");
+      api.countInVolume = 0;
+    }
+  };
+
+  const metronome = wrapper.querySelector(".at-controls .at-metronome");
+  metronome.onclick = () => {
+    metronome.classList.toggle("active");
+    if (metronome.classList.contains("active")) {
+      api.metronomeVolume = 1;
+    } else {
+      api.metronomeVolume = 0;
+    }
+  };
+
+  const loop = wrapper.querySelector(".at-controls .at-loop");
+  loop.onclick = () => {
+    loop.classList.toggle("active");
+    api.isLooping = loop.classList.contains("active");
+  };
+
+  wrapper.querySelector(".at-controls .at-print").onclick = () => {
+    api.print();
+  };
+
+  const zoom = wrapper.querySelector(".at-controls .at-zoom select");
+  zoom.onchange = () => {
+    const zoomLevel = parseInt(zoom.value) / 100;
+    api.settings.display.scale = zoomLevel;
+    api.updateSettings();
+    api.render();
+  };
+  api.settings.display.layoutMode = alphaTab.LayoutMode.Horizontal;
+
+  const layout = wrapper.querySelector(".at-controls .at-layout select");
+  layout.onchange = () => {
+    switch (layout.value) {
+      case "horizontal":
+        api.settings.display.layoutMode = alphaTab.LayoutMode.Horizontal;
+        break;
+      case "page":
+        api.settings.display.layoutMode = alphaTab.LayoutMode.Page;
+        break;
+    }
+    api.updateSettings();
+    api.render();
+  };
+
+  // player loading indicator
+  const playerIndicator = wrapper.querySelector(
+    ".at-controls .at-player-progress"
+  );
+  api.soundFontLoad.on((e) => {
+    const percentage = Math.floor((e.loaded / e.total) * 100);
+    playerIndicator.innerText = percentage + "%";
+  });
+  api.playerReady.on(() => {
+    playerIndicator.style.display = "none";
+  });
+
+  // main player controls
+  const playPause = wrapper.querySelector(".at-controls .at-player-play-pause");
+  const stop = wrapper.querySelector(".at-controls .at-player-stop");
+  playPause.onclick = (e) => {
+    if (e.target.classList.contains("disabled")) {
+      return;
+    }
+    api.playPause();
+  };
+  stop.onclick = (e) => {
+    if (e.target.classList.contains("disabled")) {
+      return;
+    }
+    api.stop();
+  };
+  api.playerReady.on(() => {
+    playPause.classList.remove("disabled");
+    stop.classList.remove("disabled");
+  });
+  api.playerStateChanged.on((e) => {
+    const icon = playPause.querySelector("i.fas");
+    if (e.state === alphaTab.synth.PlayerState.Playing) {
+      icon.classList.remove("fa-play");
+      icon.classList.add("fa-pause");
+    } else {
+      icon.classList.remove("fa-pause");
+      icon.classList.add("fa-play");
     }
   });
-});
 
-/** Controls **/
-api.scoreLoaded.on((score) => {
-  wrapper.querySelector(".at-song-title").innerText = score.title;
-  wrapper.querySelector(".at-song-artist").innerText = score.artist;
-});
-
-const countIn = wrapper.querySelector(".at-controls .at-count-in");
-countIn.onclick = () => {
-  countIn.classList.toggle("active");
-  if (countIn.classList.contains("active")) {
-    api.countInVolume = 1;
-  } else {
-    api.countInVolume = 0;
+  // song position
+  function formatDuration(milliseconds) {
+    let seconds = milliseconds / 1000;
+    const minutes = (seconds / 60) | 0;
+    seconds = (seconds - minutes * 60) | 0;
+    return (
+      String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0")
+    );
   }
-};
 
-const metronome = wrapper.querySelector(".at-controls .at-metronome");
-metronome.onclick = () => {
-  metronome.classList.toggle("active");
-  if (metronome.classList.contains("active")) {
-    api.metronomeVolume = 1;
-  } else {
-    api.metronomeVolume = 0;
-  }
-};
+  const songPosition = wrapper.querySelector(".at-song-position");
+  let previousTime = -1;
+  api.playerPositionChanged.on((e) => {
+    // reduce number of UI updates to second changes.
+    const currentSeconds = (e.currentTime / 1000) | 0;
+    if (currentSeconds == previousTime) {
+      return;
+    }
 
-const loop = wrapper.querySelector(".at-controls .at-loop");
-loop.onclick = () => {
-  loop.classList.toggle("active");
-  api.isLooping = loop.classList.contains("active");
-};
-
-wrapper.querySelector(".at-controls .at-print").onclick = () => {
-  api.print();
-};
-
-const zoom = wrapper.querySelector(".at-controls .at-zoom select");
-zoom.onchange = () => {
-  const zoomLevel = parseInt(zoom.value) / 100;
-  api.settings.display.scale = zoomLevel;
-  api.updateSettings();
-  api.render();
-};
-
-const layout = wrapper.querySelector(".at-controls .at-layout select");
-layout.onchange = () => {
-  switch (layout.value) {
-    case "horizontal":
-      api.settings.display.layoutMode = alphaTab.LayoutMode.Horizontal;
-      break;
-    case "page":
-      api.settings.display.layoutMode = alphaTab.LayoutMode.Page;
-      break;
-  }
-  api.updateSettings();
-  api.render();
-};
-
-// player loading indicator
-const playerIndicator = wrapper.querySelector(
-  ".at-controls .at-player-progress"
-);
-api.soundFontLoad.on((e) => {
-  const percentage = Math.floor((e.loaded / e.total) * 100);
-  playerIndicator.innerText = percentage + "%";
-});
-api.playerReady.on(() => {
-  playerIndicator.style.display = "none";
-});
-
-// main player controls
-const playPause = wrapper.querySelector(".at-controls .at-player-play-pause");
-const stop = wrapper.querySelector(".at-controls .at-player-stop");
-playPause.onclick = (e) => {
-  if (e.target.classList.contains("disabled")) {
-    return;
-  }
-  api.playPause();
-};
-stop.onclick = (e) => {
-  if (e.target.classList.contains("disabled")) {
-    return;
-  }
-  api.stop();
-};
-api.playerReady.on(() => {
-  playPause.classList.remove("disabled");
-  stop.classList.remove("disabled");
-});
-api.playerStateChanged.on((e) => {
-  const icon = playPause.querySelector("i.fas");
-  if (e.state === alphaTab.synth.PlayerState.Playing) {
-    icon.classList.remove("fa-play");
-    icon.classList.add("fa-pause");
-  } else {
-    icon.classList.remove("fa-pause");
-    icon.classList.add("fa-play");
-  }
-});
-
-// song position
-function formatDuration(milliseconds) {
-  let seconds = milliseconds / 1000;
-  const minutes = (seconds / 60) | 0;
-  seconds = (seconds - minutes * 60) | 0;
-  return (
-    String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0")
-  );
+    songPosition.innerText =
+      formatDuration(e.currentTime) + " / " + formatDuration(e.endTime);
+  });
 }
 
-const songPosition = wrapper.querySelector(".at-song-position");
-let previousTime = -1;
-api.playerPositionChanged.on((e) => {
-  // reduce number of UI updates to second changes.
-  const currentSeconds = (e.currentTime / 1000) | 0;
-  if (currentSeconds == previousTime) {
-    return;
-  }
+play();
 
-  songPosition.innerText =
-    formatDuration(e.currentTime) + " / " + formatDuration(e.endTime);
+var files = [
+  "./music/Happy-Birthday-To-You-easy.gpx",
+  "./music/canon.gp",
+  "./music/21gun.gp5",
+];
+
+for (let i = 0; i < files.length; i++) {
+  console.log(i);
+  let option = `
+  <option value="${files[i]}">
+    ${files[i].slice(8, files[i].length)}
+  </option>
+  `;
+  $("#music").append(option);
+}
+
+$("#music").change(() => {
+  api.settings.core.file = $("#music").val();
+  api.updateSettings();
+  play();
+  // api.render();
+  // $("#alphaTab").alphaTab("UpdateSettings");
+  // $("#alphaTab").alphaTab("render");
 });
--- testAPI.js
+++ testAPI.js
@@ -16,7 +16,7 @@ api.playedBeatChanged.on((beat) => {
     'E4' ,'F4' ,'F+4','G4' ,'G+4','A4' ,'A+4','B4' ,'C5' ,'C+5','D5' ,'D+5','E5' ,'F5' ,'F+5','G5' ,'G+5','A5' ,'A+5','B5' ,'C6' ,'C+6','D6', //String 6
     ];
   let text = "";
-  // console.log("fret:", beat.notes[0].fret, "string:", beat.notes[0].string);
+  console.log(beat.lyrics);
   for (let i = 0; i < beat.notes.length; i++) {
     let index = (beat.notes[i].string - 1) * 23 + beat.notes[i].fret;
     let link = {
